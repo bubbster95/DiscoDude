54a8c87e66458bdd748a02ca1da5955905425028
bubbster95 <stiles.billy@gmail.com>
2020-06-01 18:10:19 -0400

    Added scoring/lives, game over, restart button.

--- beats.js
+++ beats.js
@@ -1,30 +1,62 @@
-let game;
-let nextTarget;
-let speed = 10;
+let game, score = 0, lives = 2;
 let areaX, areaY;
+let target, nextTarget, spawnRate = 30, targetSpeed = 2;
 
 function init() {
     game = setInterval(gameLoop, 33);
     nextTarget = 0;
-
+    document.querySelector('.game-area > p').style.display = 'none';
     //game area dimensions
     areaY = getComputedStyle(document.querySelector('.game-area')).height
-    areaX = getComputedStyle(document.querySelector('.game-area')).width   
+    areaX = getComputedStyle(document.querySelector('.game-area')).width 
+    randomSpawn(50, 50)  
+}
+
+function gameOver() {
+    clearInterval(game);
+    document.querySelector('.game-area > p').style.display = 'block';
+    document.querySelector('.game-area > svg').style.display = 'none';
+}
+
+function restart() {
+    document.location.reload(false);
 }
     
 function gameLoop() {
+    target = document.querySelector('.game-area > button');
+    if (lives == -1) {
+        gameOver();
+    }
     nextTarget++;
-    if (nextTarget == speed) {
-        // spawnTarget(50, 50)
+    if (nextTarget == spawnRate) {
         randomSpawn(50, 50);
         nextTarget = 0;
     }
-    moveTarget(5, 0);
+    if (target) moveTarget(targetSpeed, 0);
+    if (target) target.onmousedown = () => addPoint(target);
+}
 
+function addPoint(target) {
+    target.parentNode.removeChild(target)
+    score++;
+    document.getElementById('scoreCard').innerHTML = "Score: "+ score;
+    if(targetSpeed < 20) {
+        targetSpeed+= .1;
+    }
+}
+
+function deleteTarget(target) {
+    target.parentNode.removeChild(target);
 }
 
 function moveTarget(x, y) {
-    const target = document.querySelector('.game-area > button')
+    const target = document.querySelector('.game-area > button');
+    if (parseInt(target.style.left) < parseInt(areaX)/2 + 10 &&
+        parseInt(target.style.left) > parseInt(areaX)/2 - 50){
+        deleteTarget(target);
+        document.getElementById('lives').innerHTML = "Lives: " + lives;
+        lives--;
+    }
     if (parseInt(target.style.left) > parseInt(areaX)/2) {
         target.style.left = (parseInt(target.style.left) - x) + 'px';
         target.style.top = (parseInt(target.style.top) + y) + 'px';
@@ -32,26 +64,20 @@ function moveTarget(x, y) {
         target.style.left = (parseInt(target.style.left) + x) + 'px';
         target.style.top = (parseInt(target.style.top) + y) + 'px';
     }
-    document.querySelector('.game-area').appendChild(target);
-}
-
-function spawnTarget(x, y) {
-    const target = document.createElement('BUTTON');
-    target.style.left = x + 'px';
-    target.style.top = y + 'px';
-    document.querySelector('.game-area').appendChild(target); 
+    // document.querySelector('.game-area').appendChild(target);
 }
 
 function randomSpawn(top, bottom) {
     const target = document.createElement('BUTTON');
     //toggle sides at random
     toggle = Math.random();
-    if (Math.round(toggle) == 1) target.style.left = -20 + 'px';
+    if (Math.round(toggle) == 1) target.style.left = -30 + 'px';
     else target.style.left = parseInt(areaX) + 'px';
     //spawn withing margins (top, bottom)
     target.style.top = (Math.random() * (parseInt(areaY)-top-bottom) + top) + 'px';
     
     document.querySelector('.game-area').appendChild(target);
+    // target.addEventListener('click', addPoint(target));   
 }
 
 function pause() {
@@ -64,6 +90,15 @@ function pause() {
         button.innerText = 'Pause';
     }    
 }
+
+    
+
+// function spawnTarget(x, y) {
+//     const target = document.createElement('BUTTON');
+//     target.style.left = x + 'px';
+//     target.style.top = y + 'px';
+//     document.querySelector('.game-area').appendChild(target); 
+// }
 
 // stuff flys across
     // make the stuff
