b0fb2fdf9cf9e20eba60ab50637a49ef066112a9
bubbster95 <stiles.billy@gmail.com>
2020-06-05 23:59:13 -0400

    Added slowspeed power, cleaned css page, arrow functions, fixed over spawning.

--- beats.js
+++ beats.js
@@ -2,13 +2,14 @@ let game, score, lives, hiScore = 0;
 let areaX, areaY;
 let nextTarget, spawnRate, spawnType = 0, targetSpeed;
 
-function init() {
+init = () => {
     started = false;
     game = setInterval(gameLoop, 33);
     score = 0, lives = 3, nextTarget = 0, spawnRate = 30, targetSpeed = 1,
     target = document.getElementsByClassName('target');
     bonus = document.getElementsByClassName('bonus');
     bomb = document.getElementsByClassName('bomb');
+    slow = document.getElementsByClassName('slow');
     // game area dimensions
     areaY = getComputedStyle(document.querySelector('.game-area')).height
     areaX = getComputedStyle(document.querySelector('.game-area')).width  
@@ -17,88 +18,94 @@ function init() {
     document.querySelector('.game-area > svg').style.display = 'block';
 }
     
-function gameLoop() {
+gameLoop = () => {
     document.getElementById('scoreCard').innerHTML = "Score: "+ score;
     document.getElementById('lives').innerHTML = "Lives: " + lives;
     document.getElementById('highScore').innerHTML = "High Score: "+ hiScore;
     if (started == true) {
-        nextTarget++;
-        if (nextTarget == spawnRate) {
-            spawn(50, 50);
-            nextTarget = 0;
-        }
-        // move
-        if (bonus[0]) moveTarget(bonus[0], 'bonus');
-        if (target[0]) moveTarget(target[0], 'target');
-        if (bomb[0]) moveTarget(bomb[0], 'bomb');
-        // click
-        if (target[0]) target[0].onmousedown = () => addPoint('target');
-        if (bonus[0]) bonus[0].onmousedown = () => bonusPoint('bonus');
-        if (bomb[0]) bomb[0].onmousedown = () => looseLife('bomb');
+        if (!target[0]) spawn(50, 50);
         // scores
         if (score >= hiScore) hiScore = score;
         if (score%100 == 0 && score != 0) lives++, score+= 10;
+        // move
+        if (target[0]) moveTarget(target[0], 'target');
+        if (bonus[0]) moveTarget(bonus[0], 'bonus');
+        if (bomb[0]) moveTarget(bomb[0], 'bomb');
+        if (slow[0]) moveTarget(slow[0], 'slow');
+        // click
+            if (target[0]) target[0].onmousedown = () => addPoint('target');
+            if (bonus[0]) bonus[0].onmousedown = () => bonusPoint('bonus');
+            if (bomb[0]) bomb[0].onmousedown = () => looseLife('bomb');
+            if (slow[0]) slow[0].onmousedown = () => slowMo('slow');
     }
 }
 
-function start() {
+start = () => {
     started = true;
     document.getElementById('start').style.display = 'none';
 }
 
-function spawn(top, bottom) {
-    const newTarget = document.createElement('BUTTON');
-    if (spawnType == 0) newTarget.setAttribute('class', 'target');
+spawn = (top, bottom) => {
+    const newTarget = document.createElement('img');
+    if (spawnType == 0) {
+        newTarget.setAttribute('class', 'target');
+        newTarget.src = 'assets/Target.png'
+    }
     if (spawnType == 1) {
         newTarget.setAttribute('class', 'bonus');
+        newTarget.src = 'assets/Bonus.png'
         spawnType = 0;
     } if (spawnType == 2) {
         newTarget.setAttribute('class', 'bomb');
+        newTarget.src = 'assets/Bomb.png'
+        spawnType = 0;
+    } if (spawnType == 3) {
+        newTarget.setAttribute('class', 'slow');
+        newTarget.src = 'assets/Slow.png'
         spawnType = 0;
     }
     // Spawn location
-    if (Math.round(Math.random()) == 1) newTarget.style.left = -30 + 'px';
-    else newTarget.style.left = parseInt(areaX) + 'px';
+    if (Math.round(Math.random()) == 1) newTarget.style.left = 10 - 80 + 'px';
+    else newTarget.style.left = parseInt(areaX) + 40 + 'px';
     newTarget.style.top = (Math.random() * (parseInt(areaY)-top-bottom) + top) + 'px';
     document.querySelector('.game-area').appendChild(newTarget); 
 }
 
-function moveTarget(target, name) {
+moveTarget = (target, name) => {
     let tarPos = parseInt(target.style.left);
     if (tarPos < parseInt(areaX)/2 + 30 && tarPos > parseInt(areaX)/2 - 30) {
         if (name == 'target') looseLife(name);
         if (name == 'bonus') deleteTarget(name);
-        if (name == 'bomb') { addPoint(name); 
-            if (targetSpeed > 1) targetSpeed--;
-        }
+        if (name == 'slow') deleteTarget(name);
+        if (name == 'bomb') addPoint(name); 
     }
     if (tarPos > parseInt(areaX)/2) target.style.left = tarPos - targetSpeed + 'px';
     else target.style.left = tarPos + targetSpeed + 'px';
 }
 
-function spawnChange(diceRoll) {
-    spawnType = Math.round(diceRoll * 2);
+spawnChange = (diceRoll) => {
+    spawnType = Math.ceil(diceRoll * 3);
 }
 
-function bonusPoint (className) {
+bonusPoint = (className) => {
     deleteTarget(className);
     score+=10;
 }
 
-function addPoint(className) {
+addPoint = (className) => {
     deleteTarget(className);
     score++;
     if (className == 'bomb') score++;
     if (score%20 == 0) spawnChange(Math.random());
     if(score%20 == 0 && score < 30) targetSpeed+= 1;
     if(score%10 == 0 && score > 30) targetSpeed+= 1;
-    if(score%5 == 0 && score > 60) targetSpeed+= 1;
+    if(score%10 == 0 && score > 60) targetSpeed+= 1;
     //speed cap
     if(targetSpeed > 10) targetSpeed = 10;
-    // console.log(targetSpeed)
+    console.log(targetSpeed)
 }
 
-function looseLife(className) {
+looseLife = (className) => {
     deleteTarget(className);
     if (targetSpeed > 1) targetSpeed--; 
     lives--;
@@ -107,12 +114,16 @@ function looseLife(className) {
     else lifeAnimation();
 }
 
-function lifeAnimation() {
+slowMo = (className) => {
+    if (targetSpeed > 1) targetSpeed--;
+    deleteTarget(className);
+}
+
+lifeAnimation = () => {
     let int = setInterval(frame, 33);
     let frames = 0
     function frame() {
         frames++;
-        console.log('frame', frames)
         if (frames == 10){
             clearInterval(int);
             document.querySelector('.game-area > svg').style.display = 'block';
@@ -122,12 +133,12 @@ function lifeAnimation() {
  
 }
 
-function deleteTarget(className) {
+deleteTarget = (className) => {
     const element = document.getElementsByClassName(className);
     element[0].parentNode.removeChild(element[0]);
 }
 
-function pause() {
+pause = () => {
     const button = document.querySelector('.pause-button');
     if (button.innerText == 'Pause' && lives > 0) {
         clearInterval(game);
@@ -138,7 +149,7 @@ function pause() {
     }    
 }
 
-function gameOver() {
+gameOver = () => {
     clearInterval(game);
     deleteAll('target');
     document.getElementById('game-over').innerHTML = 'Game Over!<h6 class="score">Score:'+score+'</h6>';
@@ -146,15 +157,16 @@ function gameOver() {
     document.querySelector('.game-area > svg').style.display = 'none';
 }
 
-function restart() {
+restart = () => {
     document.getElementById('start').style.display = 'block';
     clearInterval(game);
     deleteAll('target');
     deleteAll('bonus')
+    deleteAll('bomb')
     init();
 }
 
-function deleteAll(className){
+deleteAll = (className) => {
     const elements = document.getElementsByClassName(className);
     while(elements.length > 0) elements[0].parentNode.removeChild(elements[0]);
 }
@@ -171,3 +183,20 @@ function deleteAll(className){
 
 // later: slashing instead
 //fix kill zone placing to responsive
+
+// function spawn(top, bottom) {
+//     const newTarget = document.createElement('BUTTON');
+//     if (spawnType == 0) newTarget.setAttribute('class', 'target');
+//     if (spawnType == 1) {
+//         newTarget.setAttribute('class', 'bonus');
+//         spawnType = 0;
+//     } if (spawnType == 2) {
+//         newTarget.setAttribute('class', 'bomb');
+//         spawnType = 0;
+//     }
+//     // Spawn location
+//     if (Math.round(Math.random()) == 1) newTarget.style.left = -30 + 'px';
+//     else newTarget.style.left = parseInt(areaX) + 'px';
+//     newTarget.style.top = (Math.random() * (parseInt(areaY)-top-bottom) + top) + 'px';
+//     document.querySelector('.game-area').appendChild(newTarget); 
+// }
